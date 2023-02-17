308f32b66dfb437d8a3feb4bbf4e1241e89d5402
Tristin <tmille2004@gmail.com>
2023-02-17 04:58:45 -0600

    adds boxicons to sidenav

--- src/components/sidenav/SideNav.js
+++ src/components/sidenav/SideNav.js
@@ -1,14 +1,29 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import 'boxicons/css/boxicons.min.css';
+
 import "./sidenav.css"
 
 const SideNav = () => {
+  const user = useSelector((state) => state.auth.currentUser)
 
   return (
     <div className="sidenav">
       <div className="sidenav-content">
         <div className="profile-section">
-          <p className="username">Username</p>
-          <p className="email">Email</p>
+          <p className="username">{user.username}</p>
+          <p className="email">{user.email}</p>
+        </div>
+        <hr></hr>
+        <div className="files-section">
+          <Link><i class='bx bxs-folder-open' ></i>My Files</Link>
+          <Link>Other Stuff</Link>
+        </div>
+        <hr></hr>
+        <div className="settings-section">
+          <Link><i class='bx bx-user'></i>Account</Link>
+          <Link><i class='bx bx-exit'></i>Logout</Link>
         </div>
       </div>
     </div>
