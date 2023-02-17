fb3b9936d84212bf93baa0deb1294f305d3117c6
Tristin <tmille2004@gmail.com>
2023-02-17 03:52:38 -0600

    creates blank template

---
+++ src/components/sidenav/SideNav.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./sidenav.css"
+
+const SideNav = () => {
+
+  return (
+    <div className="sidenav">
+    
+    </div>
+  )
+}
+
+export default SideNav;
---
+++ src/pages/dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Dashboard = () => {
+
+  return (
+    <div className="dashboard">
+      My Files
+    </div>
+  )
+}
+
+export default Dashboard
